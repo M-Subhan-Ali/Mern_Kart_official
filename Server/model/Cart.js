ccab92dd2fff9fca039227a5df0e63a1705788e5
M-Subhan-Ali <s8289552@gmail.com>
2025-09-26 14:58:13 -0700

    created cart model

---
+++ Server/model/Cart.js
@@ -0,0 +1,29 @@
+import mongooose from "mongoose";
+
+const Cart_Schema = new mongooose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [
+      {
+        products: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: [1, "Quantity must me atleast 1"],
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Cart = new mongoose.model("Cart", Cart_Schema);
